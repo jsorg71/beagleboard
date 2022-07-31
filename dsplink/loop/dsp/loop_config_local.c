9b362a726e231967c1cc64ac72a106b183aef9ef
Jay Sorg <jay.sorg@gmail.com>
2022-07-30 21:41:34 -0700

    add loop test build

---
+++ dsplink/loop/dsp/loop_config_local.c
@@ -0,0 +1,11 @@
+
+/*  ----------------------------------- DSP/BIOS Headers            */
+#include <std.h>
+#include <sys.h>
+#include <pool.h>
+
+/*  ----------------------------------- DSP/BIOS LINK Headers       */
+#include <dsplink.h>
+
+MSGQ_Config MSGQ_config;
+
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 
 notes and scripts to build an image for beagleboard
 
+some TI tools here
+https://github.com/superna9999?tab=repositories
+
